84c9e6b0d25499b8890713e6ee43143411f029ef
Josemberg Ferreira <josembergff@gmail.com>
2023-05-23 16:09:02 -0300

    Remove default files

--- projects/jff-angular-tools/src/lib/jff-angular-tools.module.ts
+++ projects/jff-angular-tools/src/lib/jff-angular-tools.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
-import { JffAngularToolsComponent } from './jff-angular-tools.component';
-
-
 
 @NgModule({
   declarations: [
-    JffAngularToolsComponent
   ],
   imports: [
   ],
   exports: [
-    JffAngularToolsComponent
   ]
 })
 export class JffAngularToolsModule { }
--- projects/jff-angular-tools/src/public-api.ts
+++ projects/jff-angular-tools/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of jff-angular-tools
  */
 
-export * from './lib/jff-angular-tools.service';
-export * from './lib/jff-angular-tools.component';
 export * from './lib/jff-angular-tools.module';
